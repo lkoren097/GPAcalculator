e8654a316464e24ae98383f7b4f51bed569ed2e5
Lea Koren <90276118+lkoren097@users.noreply.github.com>
2021-12-15 19:54:40 -0800

    prompt step 1

--- src/GPAcalc.java
+++ src/GPAcalc.java
@@ -1,17 +1,46 @@
+/*
+ * Author: Lea Koren
+ * Date: Dec 15
+ * Prompt for Sem 1 Finals
+ */
+import java.util.Scanner;
 
 public class GPAcalc {
 
 	public static void main(String[] args) {
 		
+		 Scanner kboard = new Scanner(System.in);
 		
-		
-		
-		
-		
-		
-		
-		
+	 	 System.out.println("Please input 7 grades... ");
+		 System.out.print("Enter the first grade: ");
+		 int num1 = kboard.nextInt( );
+		    
+		 System.out.print("Enter the second grade: ");
+		 int num2 = kboard.nextInt( );
+		 
+		 System.out.print("Enter the third grade: ");
+		 int num3 = kboard.nextInt( );
+		    
+		 System.out.print("Enter the fourth grade: ");
+		 int num4 = kboard.nextInt( );
+		    
+		 System.out.print("Enter the fifth grade: ");
+		 int num5 = kboard.nextInt( );
+		    
+		 System.out.print("Enter the sixth grade: ");
+		 int num6 = kboard.nextInt( );
 
+		 System.out.print("Enter the seventh grade: ");
+		 int num7 = kboard.nextInt( );
+		    
+		    
+		    // Change this code so that it calls your method.
+		    GPAtools calc = new GPAtools();
+		    double result = calc.calcAverage(num1, num2, num3, num4, num5, num6, num7);
+
+		    //Printing out the result of the test!
+		    System.out.println("Your GPA this semester is: " + result);
+		
 	}
 
 }
---
+++ src/GPAtools.java
@@ -0,0 +1,9 @@
+
+public class GPAtools {
+
+	public double calcAverage(int num1, int num2, int num3, int num4, int num5, int num6, int num7) {
+		
+		double result = (num1 + num2 + num3 + num4 + num5 + num6 + num7) / 7.0;
+		return result;
+	}
+}
